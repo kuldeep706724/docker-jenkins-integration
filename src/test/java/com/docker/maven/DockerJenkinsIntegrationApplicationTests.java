61a9b5e0dafecd3e6f3afbdc45200169b4cbf441
kuldeep <kuldeep.shrivastava@impetus.com>
2022-05-20 18:29:08 +0530

    first commit

---
+++ src/test/java/com/docker/maven/DockerJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
